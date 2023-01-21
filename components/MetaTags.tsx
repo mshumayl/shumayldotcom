70839725b59e814e195b36904eb99264435801bd
mshumayl <mshumayl@gmail.com>
2023-01-21 23:56:15 +0800

    Fix broken og:url

--- components/MetaTags.tsx
+++ components/MetaTags.tsx
@@ -18,7 +18,7 @@ const MetaTags: React.FC<{title: string, excerpt: string, slug:string, domain: s
                 <meta property="og:image" content={`https://${domain}/api/og?title=${title}`}></meta>
                 <meta name="description" content={excerpt}></meta>
 
-                <meta property="og:url" content={`https://${domain}/pages/${slug}`}></meta>
+                <meta property="og:url" content={`https://${domain}/post/${slug}`}></meta>
                 <meta property="og:type" content="website"></meta>
                 <meta property="og:title" content={title}></meta>
                 <meta property="og:description" content={excerpt}></meta>
@@ -26,7 +26,7 @@ const MetaTags: React.FC<{title: string, excerpt: string, slug:string, domain: s
 
                 <meta name="twitter:card" content="summary_large_image"></meta>
                 <meta property="twitter:domain" content={domain}></meta>
-                <meta property="twitter:url" content={`https://${domain}/pages/${slug}`}></meta>
+                <meta property="twitter:url" content={`https://${domain}/post/${slug}`}></meta>
                 <meta name="twitter:title" content={title}></meta>
                 <meta name="twitter:description" content={excerpt}></meta>
                 <meta name="twitter:image" content={`https://${domain}/api/og?title=${title}`}></meta>
